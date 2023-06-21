182d22bb45021ac408c9bb8ad8632bc9cbb2687e
zac nation <zac.nation@icloud.com>
2023-06-21 20:51:05 +0100

    Add footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,28 @@
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import {
+  faGalacticRepublic,
+  faGithub,
+  faLinkedin,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer(props) {
   return (
     <footer className={props.darkMode ? "dark" : ""}>
-      <h1>Footer</h1>
-      <FontAwesomeIcon icon={faLinkedin} className="linkedin-icon" />
-      <FontAwesomeIcon icon={faGithub} className="github-icon" />
+      <div className="icons">
+        <FontAwesomeIcon icon={faLinkedin} className="linkedin-icon" />
+        <FontAwesomeIcon icon={faGithub} className="github-icon" />
+      </div>
+      <p>
+        Made with <FontAwesomeIcon icon={faHeart} /> (and{" "}
+        <FontAwesomeIcon icon={faReact} className="react-icon" />)
+      </p>
+      <FontAwesomeIcon
+        icon={faGalacticRepublic}
+        className="galactic-republic-icon"
+      />
     </footer>
   );
 }
