47c15d7ef79552f463063432b7dea3a752080081
zac nation <zac.nation@icloud.com>
2023-06-21 13:31:41 +0100

    Add about section without content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./index.css";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
+import About from "./components/About";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Main />
+      <About />
     </div>
   );
 }
