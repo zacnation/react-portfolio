cf497cd53e6b7d1983da3aa62366406880b33b1a
zac nation <zac.nation@icloud.com>
2023-06-22 17:47:34 +0100

    Add content to about section + images / inverted

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,62 @@
 import PropTypes from "prop-types";
-export default function About() {
+export default function About(props) {
   return (
-    <section id="one" className="about">
-      <h2>About</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-        blanditiis, reiciendis ullam voluptatum ut doloremque exercitationem,
-        suscipit accusamus asperiores voluptas quae magni? Et, ex. Dolores optio
-        beatae odit excepturi inventore.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam,
-        debitis minima sed perspiciatis fugiat facilis placeat.
-      </p>
-      <p>
-        I have recently graduated from Le Wagon, a 9-week full-time intensive
-        coding bootcamp teaching HTML, CSS, Bootstrap, Ruby, JavaScript ES6,
-        SQL, git, GitHub, Heroku, and Ruby on Rails. During the bootcamp I
-        designed, implemented, and shipped to production a clone of Airbnb and a
-        Rails prototype of Mind Mission, a mindfulness training game for 9 to
-        11-year-olds.
-      </p>
+    <section id="about" className={props.darkMode ? "dark" : ""}>
+      <div className="rows">
+        <h2>About me</h2>
+        <div className="row">
+          <p>
+            I&apos;m a graduate of Le Wagon, a 9-week full-time intensive web
+            development bootcamp based in London. During the bootcamp I worked
+            with a variety of front-end and back-end technologies. Prior to the
+            bootcamp, I spent over a year self-learning how to code, and I am
+            passionate about continuing to develop my skills in this area.
+          </p>
+          <img
+            src={
+              props.darkMode
+                ? "../../public/lewagon-logo-inverted.svg"
+                : "../../public/lewagon-logo.svg"
+            }
+            alt=""
+            className="lewagon-logo"
+          />
+        </div>
+        <div className="row">
+          <img
+            src={
+              props.darkMode
+                ? "../../public/udemy-logo-inverted.svg"
+                : "../../public/udemy-logo.svg"
+            }
+            alt=""
+            className="lewagon-logo"
+          />
+          <p>
+            Before joining Le Wagon, my self-study involved a lot of Codecademy
+            and Udemy courses. I completed A list of my completed courses can be
+            found here.
+          </p>
+        </div>
+        {/* <div className="row">
+          <p>
+            I&apos;m a graduate of Le Wagon, a 9-week full-time intensive web
+            development bootcamp based in London. During the bootcamp I worked
+            with a variety of front-end and back-end technologies. Prior to the
+            bootcamp, I spent over a year self-learning how to code, and I am
+            passionate about continuing to develop my skills in this area.
+          </p>
+          <img
+            src={
+              props.darkMode
+                ? "../../public/lewagon-logo-inverted.svg"
+                : "../../public/lewagon-logo.svg"
+            }
+            alt=""
+            className="lewagon-logo"
+          />
+        </div> */}
+      </div>
     </section>
   );
 }
