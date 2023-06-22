2145925a9a59334da979ae33d2a4b3d87a5f59f4
zac nation <zac.nation@icloud.com>
2023-06-22 20:01:28 +0100

    Add 'coming soon' to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ export default function About(props) {
   return (
     <section id="projects" className={props.darkMode ? "dark" : ""}>
       <h2>Projects</h2>
+      <h2 className="coming-soon">More coming soon 👨‍💻</h2>
     </section>
   );
 }
