91ecae902b3234f1a3e82d6a0643b3dc8ee3f112
zac nation <zac.nation@icloud.com>
2023-06-27 10:39:53 +0100

    Begin navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,66 +1,105 @@
 import PropTypes from "prop-types";
-
 export default function Navbar(props) {
   return (
     <nav
-      className={`flex items-center justify-between p-4 ${
-        props.darkMode ? "bg-gray-900 text-white" : "bg-white text-black"
+      className={`flex items-center justify-between ${
+        props.darkMode ? "dark" : ""
       }`}
     >
-      <h3 className="text-xl font-bold">Zac Nation</h3>
-      <div className="flex items-center">
-        <div
-          className={`relative w-12 h-6 rounded-full bg-gray-400 ${
-            props.darkMode ? "bg-blue-600" : "bg-gray-300"
-          }`}
-          onClick={props.toggleDarkMode}
-        >
-          <div
-            className={`absolute w-4 h-4 rounded-full bg-white ${
-              props.darkMode ? "left-6" : "left-1"
-            }`}
-          ></div>
+      <h3 className="logo">Zac Nation</h3>
+      <div className="nav-elements">
+        <div className="toggler">
+          <div className="toggler--slider" onClick={props.toggleDarkMode}>
+            <div className="toggler--slider--circle"></div>
+          </div>
         </div>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
       </div>
-      <ul className="hidden sm:flex">
-        <li>
-          <a
-            href="#home"
-            className="px-2 py-1 text-gray-800 hover:text-gray-600"
-          >
-            Home
-          </a>
-        </li>
-        <li>
-          <a
-            href="#about"
-            className="px-2 py-1 text-gray-800 hover:text-gray-600"
-          >
-            About
-          </a>
-        </li>
-        <li>
-          <a
-            href="#projects"
-            className="px-2 py-1 text-gray-800 hover:text-gray-600"
-          >
-            Projects
-          </a>
-        </li>
-        <li>
-          <a
-            href="#contact"
-            className="px-2 py-1 text-gray-800 hover:text-gray-600"
-          >
-            Contact
-          </a>
-        </li>
-      </ul>
     </nav>
   );
 }
 
 Navbar.propTypes = {
   darkMode: PropTypes.bool.isRequired,
-  toggleDarkMode: PropTypes.func.isRequired,
+  toggleDarkMode: PropTypes.bool.isRequired,
 };
+
+// import PropTypes from "prop-types";
+
+// export default function Navbar(props) {
+//   return (
+//     <nav
+//       className={`flex items-center justify-between p-4 ${
+//         props.darkMode ? "bg-gray-900 text-white" : "bg-white text-black"
+//       }`}
+//     >
+//       <h3 className="text-xl font-bold">Zac Nation</h3>
+//       <div className="flex items-center">
+//         <div
+//           className={`relative w-12 h-6 rounded-full bg-gray-400 ${
+//             props.darkMode ? "bg-blue-600" : "bg-gray-300"
+//           }`}
+//           onClick={props.toggleDarkMode}
+//         >
+//           <div
+//             className={`absolute w-4 h-4 rounded-full bg-white ${
+//               props.darkMode ? "left-6" : "left-1"
+//             }`}
+//           ></div>
+//         </div>
+//       </div>
+//       <ul className="hidden sm:flex">
+//         <li>
+//           <a
+//             href="#home"
+//             className="px-2 py-1 text-gray-800 hover:text-gray-600"
+//           >
+//             Home
+//           </a>
+//         </li>
+//         <li>
+//           <a
+//             href="#about"
+//             className="px-2 py-1 text-gray-800 hover:text-gray-600"
+//           >
+//             About
+//           </a>
+//         </li>
+//         <li>
+//           <a
+//             href="#projects"
+//             className="px-2 py-1 text-gray-800 hover:text-gray-600"
+//           >
+//             Projects
+//           </a>
+//         </li>
+//         <li>
+//           <a
+//             href="#contact"
+//             className="px-2 py-1 text-gray-800 hover:text-gray-600"
+//           >
+//             Contact
+//           </a>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// }
+
+// Navbar.propTypes = {
+//   darkMode: PropTypes.bool.isRequired,
+//   toggleDarkMode: PropTypes.func.isRequired,
+// };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        monospace: ["Courier New", "Courier", "monospace"],
+      },
+    },
   },
   plugins: [],
 };
