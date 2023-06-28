88dac90ad63faa5ef649154374f2fa1e4329cd2e
zac nation <zac.nation@icloud.com>
2023-06-28 10:02:04 +0100

    Mobile-first main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,20 +2,27 @@ import PropTypes from "prop-types";
 
 export default function Main(props) {
   return (
-    <main className={`${props.darkMode ? "bg-dark text-white" : "bg-light"}`}>
-      <div className="container">
-        <div className="content">
-          <h1>Hi, I&apos;m Zac.</h1>
-          <p className="bio">A Full-Stack Developer based in London.</p>
-          <div className="tech-stack">
-            <div className="mern">
-              <p>Tech Stack |</p>
+    <main
+      className={`py-20 lg:w-screen lg:h-screen ${
+        props.darkMode ? "bg-dark text-white" : "bg-light"
+      }`}
+    >
+      <div className="">
+        <div className="">
+          <h1 className="text-4xl font-bold text-center md:text-6xl">
+            Hi, I&apos;m Zac.
+          </h1>
+          <p className="text-sm md:text-lg text-center mt-3">
+            A Full-Stack Developer based in London.
+          </p>
+          <div className="flex flex-col text-center">
+            <div className="mern text-5xl space-x-3 my-3">
               <i className="devicon-mongodb-plain"></i>
               <i className="devicon-express-original"></i>
               <i className="devicon-react-original"></i>
               <i className="devicon-nodejs-plain"></i>
             </div>
-            <div className="not-mern">
+            <div className="not-mern text-3xl space-x-2.5">
               <i className="devicon-html5-plain"></i>
               <i className="devicon-css3-plain"></i>
               <i className="devicon-bootstrap-plain"></i>
