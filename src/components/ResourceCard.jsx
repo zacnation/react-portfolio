b71f444491a2f98a37f44b359f1ff3a515c57a03
zac nation <zac.nation@icloud.com>
2023-06-29 18:53:36 +0100

    Tablet 768 - 'resources and ResourceCard'

--- src/components/ResourceCard.jsx
+++ src/components/ResourceCard.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 
 export default function ResourceCard(props) {
   return (
-    <div className="flex py-4">
-      <img src={props.imageSrc} className={`w-10 pr-2`}></img>
-      <ul>
+    <div className="flex py-4 px-1">
+      <img src={props.imageSrc} className={`w-10 pr-2 md:pr-4 md:w-12`}></img>
+      <ul className="md:w-24">
         {props.listItems.map((item, index) => {
           return (
             <li className="text-xs" key={index}>
--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -4,16 +4,16 @@ import ResourceCard from "./ResourceCard";
 export default function Resources(props) {
   return (
     <section
-      className={`pt-8 px-3 ${
+      className={`pt-8 px-3 md:px-10 ${
         props.darkMode ? "bg-very-dark text-white" : "bg-white"
       }`}
     >
-      <h2 className="md:hidden text-2xl font-bold pb-5">Resources</h2>
-      <h2 className="hidden md:block text-2xl font-bold">
+      <h2 className="lg:hidden text-2xl font-bold pb-5">Resources</h2>
+      <h2 className="hidden lg:block text-2xl font-bold">
         Resources I&apos;m incredibly grateful for
       </h2>
 
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 md:hidden">
         <div className="flex items-center">
           <ResourceCard
             imageSrc={
@@ -55,6 +55,60 @@ export default function Resources(props) {
         </div>
 
         <div className="grid col-span-3">
+          <ResourceCard
+            imageSrc={
+              props.darkMode ? "youtube-dark-f.svg" : "youtube-icon.svg"
+            }
+            listItems={[
+              "danascript",
+              "Tiff In Tech",
+              "Tina Huang",
+              "FreeCodeCamp",
+            ]}
+          />
+        </div>
+      </div>
+
+      <div className="hidden md:flex justify-center">
+        <div className="flex items-center">
+          <ResourceCard
+            imageSrc={
+              props.darkMode ? "lewagon-icon-inverted.svg" : "lewagon-icon.svg"
+            }
+            listItems={["The entire team 🥂"]}
+          />
+        </div>
+
+        <div className="flex items-center">
+          <ResourceCard
+            imageSrc={
+              props.darkMode ? "scrimba-dark-f.svg" : "scrimba-icon.svg"
+            }
+            listItems={["The Scrimba podcast", "The free catalogue"]}
+          />
+        </div>
+
+        <div className="flex items-center">
+          <ResourceCard
+            imageSrc={
+              props.darkMode ? "udemy-icon-inverted.svg" : "udemy-icon.svg"
+            }
+            listItems={["Angela Yu", "Colt Steele"]}
+          />
+        </div>
+
+        <div className="flex items-center">
+          <ResourceCard
+            imageSrc={
+              props.darkMode
+                ? "codecademy-icon-inverted.svg"
+                : "codecademy-icon.svg"
+            }
+            listItems={["Full-Stack career path"]}
+          />
+        </div>
+
+        <div className="flex items-center">
           <ResourceCard
             imageSrc={
               props.darkMode ? "youtube-dark-f.svg" : "youtube-icon.svg"
