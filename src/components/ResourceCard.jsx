044e145f111e568b17b73b9d67e19393646b8cbc
zac nation <zac.nation@icloud.com>
2023-06-30 00:29:25 +0100

    Make 'resources' spacing match others + increase font size

--- src/components/ResourceCard.jsx
+++ src/components/ResourceCard.jsx
@@ -4,10 +4,10 @@ export default function ResourceCard(props) {
   return (
     <div className="flex py-4 px-1">
       <img src={props.imageSrc} className={`w-10 pr-2 md:pr-4 md:w-12`}></img>
-      <ul className="md:w-24">
+      <ul className="md:w-24 lg:w-40">
         {props.listItems.map((item, index) => {
           return (
-            <li className="text-xs" key={index}>
+            <li className="text-xs lg:text-sm" key={index}>
               {item}
             </li>
           );
