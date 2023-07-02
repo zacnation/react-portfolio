474a1f89a186f34a3a9e39ec96ef15d0c4de1ac8
zac nation <zac.nation@icloud.com>
2023-07-02 23:00:02 +0100

    Change ProjectCard design to backgroundImg partial

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,54 +1,62 @@
 import PropTypes from "prop-types";
 
+// export default function ProjectCard(props) {
+//   return (
+//     <div
+//       className={`flex flex-col md:flex-row mb-20 py-5 px-1 rounded-md shadow-lg items-center ${
+//         props.darkMode
+//           ? "bg-dark-II text-white shadow-very-dark"
+//           : "bg-white shadow-gray-100"
+//       }`}
+//     >
+//       <h2 className="text-3xl pb-4 md:pb-0">{props.title}</h2>
+//       <p className="text-justify px-5 font-thin">{props.description}</p>
+//       <div
+//         className={`flex items-center space-x-2 justify-center ${
+//           props.darkMode ? "text-white" : "text-black"
+//         }`}
+//       >
+//         {props.techs.map((item, index) => {
+//           return item === "Stimulus" ? (
+//             <img
+//               src={
+//                 props.darkMode
+//                   ? "../../dist/stimulus-svgrepo-com.svg"
+//                   : "https://www.svgrepo.com/show/354392/stimulus.svg"
+//               }
+//               alt=""
+//               className="w-6 h-6"
+//             />
+//           ) : (
+//             <i
+//               className={`text-2xl devicon-${item.toLowerCase()}-${
+//                 item === "Express" ? "original" : "plain"
+//               }`}
+//               key={index}
+//             ></i>
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+// }
+
 export default function ProjectCard(props) {
   return (
     <div
-      className={`flex flex-col md:flex-row mb-20 py-5 px-1 rounded-md shadow-lg ${
-        props.darkMode
-          ? "bg-dark-II text-white shadow-very-dark"
-          : "bg-white shadow-gray-100"
-      }`}
+      className="flex items-center bg-cover bg-center h-44 filter grayscale hover:filter-none cursor-pointer"
+      style={{
+        backgroundImage: `url(${props.imgSrc})`,
+      }}
     >
-      <h2 className="text-3xl pb-4 md:pb-0">{props.title}</h2>
-      <p className="text-justify px-5 font-thin">{props.description}</p>
-      <div
-        className={`flex items-center space-x-2 justify-center ${
-          props.darkMode ? "text-white" : "text-black"
-        }`}
-      >
-        {props.techs.map((item, index) => {
-          return item === "Stimulus" ? (
-            <img
-              src={
-                props.darkMode
-                  ? "../../dist/stimulus-svgrepo-com.svg"
-                  : "https://www.svgrepo.com/show/354392/stimulus.svg"
-              }
-              alt=""
-              className="w-6 h-6"
-            />
-          ) : (
-            <i
-              className={`text-2xl devicon-${item.toLowerCase()}-${
-                item === "Express" ? "original" : "plain"
-              }`}
-              key={index}
-            ></i>
-          );
-        })}
-      </div>
+      <h1 className="text-white text-4xl mx-auto">{props.title}</h1>
     </div>
   );
 }
 
-// src={
-//   props.darkMode
-//     ? "lewagon-logo-white.svg"
-//     : "lewagon-logo-black.svg"
-// }
-
 ProjectCard.propTypes = {
   darkMode: PropTypes.bool.isRequired,
+  imgSrc: PropTypes.string,
   title: PropTypes.string,
   description: PropTypes.string,
   techs: PropTypes.array,
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,16 +13,19 @@ export default function About(props) {
       <div className="font-bold text-center py-28 md:py-40 lg:py-60">
         <ProjectCard
           darkMode={props.darkMode}
-          title="MIND MISSION"
+          imgSrc={
+            "https://troopl-portfolio.s3.eu-west-1.amazonaws.com/8va80qj5zxv7ddk4b4j3f022zp1i?response-content-disposition=inline%3B%20filename%3D%22Screenshot%202023-03-26%20at%2022.31.08.png%22%3B%20filename%2A%3DUTF-8%27%27Screenshot%25202023-03-26%2520at%252022.31.08.png&response-content-type=image%2Fpng&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJVWH45TXBJODL24A%2F20230702%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20230702T212511Z&X-Amz-Expires=300&X-Amz-SignedHeaders=host&X-Amz-Signature=64eda91c1d12df0635eda57f8e20ead3a38bca8790d1572da2efc4c5b0217e25"
+          }
+          title=""
           description="A Rails prototype of Mind Mission, a mindfulness training game for 9 to 11-year-olds. Sed sagittis felis turpis, vitae accumsan nibh lacinia in. Nulla nec tellus at sapien fringilla consequat. Cras ac ultrices neque. Suspendisse vehicula pretium maximus. Phasellus lorem risus, semper nec mauris et, luctus convallis nisi. Integer vel leo at erat dictum pharetra. Suspendisse efficitur lacus odio, nec porta massa mollis sit amet."
           techs={["Ruby", "Rails", "JavaScript", "Stimulus"]}
         />
-        <ProjectCard
+        {/* <ProjectCard
           darkMode={props.darkMode}
           title="PORTFOLIO"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dui augue, gravida ut nisl ut, consectetur sollicitudin felis. Duis ac ex id turpis molestie accumsan. Curabitur a auctor neque. Suspendisse laoreet, lacus sit amet vehicula tristique, eros mauris lacinia arcu, vitae vehicula nunc risus quis libero. Cras et quam mauris."
           techs={["html5", "css3", "tailwindcss", "javascript", "react"]}
-        />
+        /> */}
       </div>
     </section>
   );
