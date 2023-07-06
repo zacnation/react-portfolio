7b1aa8a55a707c660f90117385416f9b63dc1168
zac nation <zac.nation@icloud.com>
2023-07-06 11:56:53 +0100

    Add inverted ProjectCard, and dummy projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import ProjectCard from "./ProjectCard";
+import ProjectCardInverted from "./ProjectCardInverted";
 
 export default function About(props) {
   return (
@@ -17,12 +18,43 @@ export default function About(props) {
           description="A Rails prototype of Mind Mission, a mindfulness training game for 9 to 11-year-olds. Sed sagittis felis turpis, vitae accumsan nibh lacinia in. Nulla nec tellus at sapien fringilla consequat. Cras ac ultrices neque. Suspendisse vehicula pretium maximus. Phasellus lorem risus, semper nec mauris et, luctus convallis nisi. Integer vel leo at erat dictum pharetra. Suspendisse efficitur lacus odio, nec porta massa mollis sit amet."
           techs={["ruby", "rails", "javascript", "stimulus"]}
         />
-        <ProjectCard
-          invert={true}
+        <ProjectCardInverted
           darkMode={props.darkMode}
           title="PORTFOLIO"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dui augue, gravida ut nisl ut, consectetur sollicitudin felis. Duis ac ex id turpis molestie accumsan. Curabitur a auctor neque. Suspendisse laoreet, lacus sit amet vehicula tristique, eros mauris lacinia arcu, vitae vehicula nunc risus quis libero. Cras et quam mauris."
-          techs={["html5", "css3", "tailwindcss", "javascript", "react"]}
+          techs={["html5", "tailwindcss", "javascript", "react"]}
+        />
+        <ProjectCard
+          darkMode={props.darkMode}
+          title="LIGHTS OUT"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dui augue, gravida ut nisl ut, consectetur sollicitudin felis. Duis ac ex id turpis molestie accumsan. Curabitur a auctor neque. Suspendisse laoreet, lacus sit amet vehicula tristique, eros mauris lacinia arcu, vitae vehicula nunc risus quis libero. Cras et quam mauris."
+          techs={["html5", "css3", "javascript", "react"]}
+        />
+        <ProjectCardInverted
+          darkMode={props.darkMode}
+          title="FIREBASE TO-DO"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dui augue, gravida ut nisl ut, consectetur sollicitudin felis. Duis ac ex id turpis molestie accumsan. Curabitur a auctor neque. Suspendisse laoreet, lacus sit amet vehicula tristique, eros mauris lacinia arcu, vitae vehicula nunc risus quis libero. Cras et quam mauris."
+          techs={["html5", "css3", "javascript", "firebase"]}
+        />
+        <ProjectCard
+          darkMode={props.darkMode}
+          title="YELPCAMP"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dui augue, gravida ut nisl ut, consectetur sollicitudin felis. Duis ac ex id turpis molestie accumsan. Curabitur a auctor neque. Suspendisse laoreet, lacus sit amet vehicula tristique, eros mauris lacinia arcu, vitae vehicula nunc risus quis libero. Cras et quam mauris."
+          techs={[
+            "html5",
+            "css3",
+            "bootstrap",
+            "javascript",
+            "mongodb",
+            "express",
+            "nodejs",
+          ]}
+        />
+        <ProjectCardInverted
+          darkMode={props.darkMode}
+          title="AIRBNB"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dui augue, gravida ut nisl ut, consectetur sollicitudin felis. Duis ac ex id turpis molestie accumsan. Curabitur a auctor neque. Suspendisse laoreet, lacus sit amet vehicula tristique, eros mauris lacinia arcu, vitae vehicula nunc risus quis libero. Cras et quam mauris."
+          techs={["html5", "css3", "bootstrap", "javascript", "ruby", "rails"]}
         />
       </div>
     </section>
