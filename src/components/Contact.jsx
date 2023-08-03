38573239f458a8d9bcf5e5ff6968fae678bde8fb
zac nation <zac.nation@icloud.com>
2023-08-03 12:38:39 +0100

    Contact?

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,10 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 export default function Resources(props) {
   return (
     <div
       className={`pt-8 px-3 md:px-10 xl:px-60 2xl:px-96 ${
-        props.darkMode ? "bg-dark-I text-white" : "bg-white"
+        props.darkMode ? 'bg-dark-I text-white' : 'bg-white'
       }`}
     ></div>
   );
