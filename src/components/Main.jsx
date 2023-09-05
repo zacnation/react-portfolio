e3ca0d3040526f811f9f4e3c566bb40ddcb17ae9
zac nation <zac.nation@icloud.com>
2023-09-05 22:45:32 +0100

    Refactor Main for small screens + change tech stack

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,9 +12,14 @@ export default function Main(props) {
         <h1 className="text-4xl font-bold text-center md:text-6xl lg:text-7xl xl:text-8xl xl:tracking-tighter">
           Hi, I&apos;m Zac.
         </h1>
-        <p className="text-sm md:text-lg text-center mt-3 xl:text-xl">
+        <p className="hidden text-sm md:block md:text-lg text-center mt-3 xl:text-xl">
           A Full-Stack Web Developer based in London.
         </p>
+        <p className="text-sm md:hidden text-center mt-3">
+          A Full-Stack Web Developer
+        </p>
+        <p className="text-sm md:hidden text-center mt-3">based in London.</p>
+
         <div className="flex flex-col text-center">
           <div className="mern text-5xl space-x-3 my-3">
             <i className="devicon-mongodb-plain"></i>
@@ -22,16 +27,17 @@ export default function Main(props) {
             <i className="devicon-react-original"></i>
             <i className="devicon-nodejs-plain"></i>
           </div>
-          <div className="not-mern text-3xl max-[350px]:text-2xl space-x-2.5">
+          <div className="not-mern text-3xl max-[350px]:text-2xl space-x-4">
             <i className="devicon-html5-plain"></i>
             <i className="devicon-css3-plain"></i>
             <i className="devicon-tailwindcss-plain"></i>
+            <i className="devicon-bootstrap-plain"></i>
             <i className="devicon-javascript-plain"></i>
-            <i className="devicon-typescript-plain"></i>
+            {/* <i className="devicon-typescript-plain"></i> */}
             <i className="devicon-ruby-plain"></i>
             <i className="devicon-rails-plain"></i>
-            <i className="devicon-postgresql-plain"></i>
-            <i className="devicon-jest-plain"></i>
+            {/* <i className="devicon-postgresql-plain"></i> */}
+            {/* <i className="devicon-jest-plain"></i> */}
           </div>
         </div>
       </div>
