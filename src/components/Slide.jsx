c6d1716f38b5176822a5976f5241807bb646b30a
zac nation <zac.nation@icloud.com>
2023-09-17 20:20:39 +0100

    Make images larger on small screens

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -5,54 +5,62 @@ import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 export default function Slide(props) {
   return (
-    <div className="px-12 md:p-0 md:flex md:align-center md:justify-center">
-      <div>
-        <img src={props.img} alt="" style={{ width: '400px' }} />
-      </div>
-      <div className="md:ml-9">
-        {props.title === 'Portfolio' ? (
-          <h2 className="text-xl font-bold md:pt-0">Portfolio</h2>
-        ) : (
-          <div className="flex items-center max-md:justify-center space-x-2 pt-2 hover:underline underline-offset-2">
-            <a
-              href={props.link}
-              rel="noreferrer"
-              target={'_blank'}
-              className="text-xl font-bold md:pt-0"
-            >
-              {props.title}{' '}
-            </a>
-            <FontAwesomeIcon
-              icon={faArrowUpRightFromSquare}
-              className="w-6 ml-2"
-            />
-          </div>
-        )}
-
-        <div className="flex items-center justify-center space-x-2 md:flex md:justify-start md:py-3 max-md:py-4">
-          {props.techs.map((item, index) => {
-            return item === 'stimulus' ? (
-              <img
-                src="stimulus.png"
-                alt=""
-                className="w-6 h-6 inline"
-                key={index}
-              />
-            ) : (
-              <i
-                className={`text-2xl devicon-${item}-${
-                  item === 'express' ? 'original' : 'plain'
-                }`}
-                key={index}
-              ></i>
-            );
-          })}
+    <>
+      <img src={props.img} alt="" className="md:hidden" />
+      <div className="px-12 md:p-0 md:flex md:align-center md:justify-center">
+        <div>
+          <img
+            src={props.img}
+            alt=""
+            style={{ width: '400px' }}
+            className="max-md:hidden"
+          />
         </div>
-        <p className="md:w-96 overflow-y-auto max-sm:h-80 h-44 border rounded p-2">
-          {props.description}
-        </p>
+        <div className="md:ml-9">
+          {props.title === 'Portfolio' ? (
+            <h2 className="text-xl font-bold md:pt-0">Portfolio</h2>
+          ) : (
+            <div className="flex items-center max-md:justify-center space-x-2 pt-2 hover:underline underline-offset-2">
+              <a
+                href={props.link}
+                rel="noreferrer"
+                target={'_blank'}
+                className="text-xl font-bold md:pt-0"
+              >
+                {props.title}{' '}
+              </a>
+              <FontAwesomeIcon
+                icon={faArrowUpRightFromSquare}
+                className="w-6 ml-2"
+              />
+            </div>
+          )}
+
+          <div className="flex items-center justify-center space-x-2 md:flex md:justify-start md:py-3 max-md:py-4">
+            {props.techs.map((item, index) => {
+              return item === 'stimulus' ? (
+                <img
+                  src="stimulus.png"
+                  alt=""
+                  className="w-6 h-6 inline"
+                  key={index}
+                />
+              ) : (
+                <i
+                  className={`text-2xl devicon-${item}-${
+                    item === 'express' ? 'original' : 'plain'
+                  }`}
+                  key={index}
+                ></i>
+              );
+            })}
+          </div>
+          <p className="md:w-96 overflow-y-auto max-sm:h-80 h-44 border rounded p-2">
+            {props.description}
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
