d4aba7065d3886fc41a6781037163b41cf64e577
zac nation <zac.nation@icloud.com>
2023-09-17 21:41:34 +0100

    Improve Email button to render 'Copied' for 1.5 seconds

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,28 +1,43 @@
+import React, { useState } from 'react'; // Import useState
 import PropTypes from 'prop-types';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function ContactCard(props) {
+  const [copied, setCopied] = useState(false); // Initialize a state variable
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText('zac.nation.work@outlook.com');
+    setCopied(true); // Set copied to true when the div is clicked
+
+    setTimeout(() => {
+      setCopied(false); // Reset copied to false after 2 seconds
+    }, 1500);
+  };
+
   return (
     <>
       {props.title === 'Email' ? (
         <div
-          onClick={() =>
-            navigator.clipboard.writeText('zac.nation.work@outlook.com')
-          }
+          onClick={handleCopyClick}
           className={`transition-all duration-150 flex items-center justify-center border rounded w-40 h-14 hover:cursor-pointer  ${
             props.darkMode
               ? 'bg-dark-I text-white border-gray-400 hover:border-white'
               : 'bg-white text-black border-gray-400 hover:border-dark-I'
           }`}
         >
-          <FontAwesomeIcon icon={props.icon} className="w-7 h-7 pr-2" />{' '}
-          <p className="text-lg">{props.title}</p>{' '}
-          <img
-            src={props.darkMode ? 'link-inverted.svg' : 'link.svg'}
-            className="w-7 pl-1 animate-pulse"
-            alt=""
-          />
+          {copied ? (
+            <p className="text-lg">Copied!</p>
+          ) : (
+            <>
+              <FontAwesomeIcon icon={props.icon} className="w-7 h-7 pr-2" />
+              <p className="text-lg">{copied ? 'Copied!' : 'Email'}</p>
+              <img
+                src={props.darkMode ? 'link-inverted.svg' : 'link.svg'}
+                className="w-7 pl-1 animate-pulse"
+                alt=""
+              />
+            </>
+          )}
         </div>
       ) : (
         <a
