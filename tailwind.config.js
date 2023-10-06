e800af9562f0653a95291a4964f30d31db2d738e
zac nation <zac.nation@icloud.com>
2023-10-06 21:47:08 +0100

    Edit colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
-        monospace: ["Courier New", "Courier", "monospace"],
+        monospace: ['Courier New', 'Courier', 'monospace'],
       },
       colors: {
-        light: "#f8f8f8",
-        "dark-I": "#282d35",
-        "dark-II": "#232930",
-        "very-dark": "#21222a",
-        dark: "#1A202C",
-        "dark-text": "#FFFFFF",
+        light: '#f8f8f8',
+        'dark-I': '#282d35',
+        'dark-II': '#232930',
+        'very-dark': '#21222a',
+        dark: '#1A202C',
+        'dark-text': '#FFFFFF',
       },
       transitionProperty: {
-        colors: "background-color, color",
+        colors: 'background-color, color',
       },
     },
   },
