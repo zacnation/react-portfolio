a4c1895d170cbac8fb5b17a8744f57518a02d3bd
Zac Nation <zac.nation@icloud.com>
2023-10-27 18:26:35 +0100

    Implement 'show more' to Interests

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -4,9 +4,8 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import InterestsSlide from "./InterestsSlide";
-import InterestsModal from "./InterestsModal";
 
-export default function Interests({ darkMode, openModal, closeModal }) {
+export default function Interests({ darkMode }) {
   const swiperOptions = {
     loop: true,
   };
@@ -39,7 +38,7 @@ export default function Interests({ darkMode, openModal, closeModal }) {
         </SwiperSlide>
         <SwiperSlide>
           <InterestsSlide
-            title={"BSL"}
+            title={"British Sign Language (BSL)"}
             description={
               "British Sign Language (BSL): Learning BSL isn't just a personal interest; it's my way of embracing two critical aspects of web development: accessibility and diversity, equity, and inclusion (DEI). BSL is part of the bigger picture of making the digital world more inclusive for those who rely on sign language. This experience has given me valuable insights into the challenges faced by individuals with disabilities, inspiring me to design websites and applications that cater to their needs. It's also a reminder that diversity includes linguistic diversity, which I'm passionate about promoting in the tech industry."
             }
@@ -53,24 +52,11 @@ export default function Interests({ darkMode, openModal, closeModal }) {
             description={
               "Gaining skills such as being able to work with the terminal, using git and GitHub, and deploying with Netlify / Cyclic has been great. Next, I want to learn more about the cloud and AWS, as well as how to work with Docker and Kubernetes, work with a Raspberry Pi for IoT projects, and continue to learn more about areas such as cybersecurity and data structures and algorithms. I feel even having a foundational understanding of these areas will help me become a better developer."
             }
-            img={`${darkMode ? "books-inverted.png" : "books.png"}`}
+            img={"version-control.jpg"}
             darkMode={darkMode}
           />
         </SwiperSlide>
       </Swiper>
-      <InterestsModal
-        title={"Reading is a passion of mine, and my book collection..."}
-        description={
-          "Reading is a passion of mine, and my book collection spans a wide range of genres and topics. From personal development works like 'How to Win Friends and Influence People', 'Atomic Habits', and 'Deep Work' to timeless classics like '1984', 'The Trial', and 'The Master and Margarita', I find inspiration in the diverse perspectives that literature offers. These books have not only fueled my creativity and problem-solving skills but have also enriched my understanding of human behavior and interaction."
-        }
-        darkMode={darkMode}
-        urlSrc={
-          "https://www.goodreads.com/review/list/169529187-zac?shelf=read"
-        }
-        linkText={"My previously read"}
-        onOpen={openModal}
-        onClose={closeModal}
-      />
     </div>
   );
 }
--- src/components/InterestsSlide.jsx
+++ src/components/InterestsSlide.jsx
@@ -1,57 +1,49 @@
 import PropTypes from "prop-types";
-import InterestsModal from "./InterestsModal";
+import { useState } from "react";
 
-export default function Slide(props) {
+export default function InterestsSlide(props) {
+  const [showMore, setShowMore] = useState(false);
   return (
     <>
+      <h2 className={`text-2xl font-bold text-center md:pt-0 lg:pb-1`}>
+        {props.title}
+      </h2>
+      <img src={props.img} alt="" className="lg:hidden pt-8 md:px-40 pb-4" />
       <div className="px-12 space-x-10 md:p-0 md:flex md:items-center md:justify-center">
         <div>
-          <h2
-            className={`text-3xl font-bold text-center md:pt-0 lg:pb-1 ${
-              props.link && "hover:underline underline-offset-2"
-            }`}
+          {/* <h2
+            className={`text-3xl font-bold text-center max-lg:hidden md:pt-0 lg:pb-1`}
           >
             {props.title}
-          </h2>
-          <div className="md:ml-9">
-            {/* <p className="w-full md:w-96 overflow-y-auto max-sm:h-80 h-min max-h-60 border rounded py-2 px-4">
-              {props.description}
-            </p> */}
-            <InterestsModal
-              title={"Reading is a passion of mine, and my book collection..."}
-              description={
-                "Reading is a passion of mine, and my book collection spans a wide range of genres and topics. From personal development works like 'How to Win Friends and Influence People', 'Atomic Habits', and 'Deep Work' to timeless classics like '1984', 'The Trial', and 'The Master and Margarita', I find inspiration in the diverse perspectives that literature offers. These books have not only fueled my creativity and problem-solving skills but have also enriched my understanding of human behavior and interaction."
-              }
-              darkMode={props.darkMode}
-              urlSrc={
-                "https://www.goodreads.com/review/list/169529187-zac?shelf=read"
-              }
-              linkText={"My previously read"}
-              onOpen={props.openModal}
-              onClose={props.closeModal}
-            />
+          </h2> */}
+          <div className="flex flex-col justify-top md:ml-9">
+            <p className="w-full md:w-96 overflow-y-auto h-min max-h-60 border rounded py-2 px-4">
+              {showMore
+                ? props.description + " "
+                : props.description.slice(0, 150) + "... "}
+            </p>
+            <button
+              className="font-bold pt-2"
+              onClick={() => setShowMore(!showMore)}
+            >
+              {showMore ? "Show less" : "Show more"}
+            </button>
           </div>
         </div>
         <img
           src={props.img}
           alt=""
           style={{ width: "30%", height: "30%" }}
-          className="max-lg:hidden pt-6"
+          className="max-lg:hidden py-8"
         />
       </div>
-      <img src={props.img} alt="" className="lg:hidden md:px-40" />
     </>
   );
 }
 
-Slide.propTypes = {
+InterestsSlide.propTypes = {
   darkMode: PropTypes.bool,
   img: PropTypes.string,
   title: PropTypes.string,
   description: PropTypes.string,
-  // techs: PropTypes.arrayOf(PropTypes.string),
-  // link: PropTypes.string,
-  // email: PropTypes.string,
-  // password: PropTypes.string,
-  // badge: PropTypes.string,
 };
