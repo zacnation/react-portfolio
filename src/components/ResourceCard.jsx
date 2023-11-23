def1788e3574e9c33f008dc0ff1b28b6e736efb4
Zac Nation <zac.nation@icloud.com>
2023-11-23 15:58:42 +0000

    Make resources horizontal

--- src/components/ResourceCard.jsx
+++ src/components/ResourceCard.jsx
@@ -1,11 +1,11 @@
 export default function ResourceCard(props) {
   return (
-    <div className="flex flex-col items-center justify-center py-4 px-1 text-center">
-      <img src={props.imageSrc} className={`w-8 pb-3 md:w-8`}></img>
-      <ul className="md:w-24 lg:w-40">
+    <div className="flex items-center justify-center py-4 px-1 text-center">
+      <img src={props.imageSrc} className={`w-8`}></img>
+      <ul>
         {props.listItems.map((item, index) => {
           return (
-            <li className="text-xs lg:text-sm" key={index}>
+            <li className="text-xs lg:text-sm  md:ml-2 md:mr-6" key={index}>
               {item}
             </li>
           );
